d2f7e43abcfd94a060bbe762dee2a2cbb11d5661
git-suraj <reach.suraj777@gmail.com>
2019-08-19 08:34:55 +0530

    added metadata

---
+++ activity/appendarraysv1/activity_metadata.go
@@ -0,0 +1,44 @@
+package appendarraysv1
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+var jsonMetadata = `{
+    "name": "tibco-append-arrays",
+    "type": "flogo:activity",
+    "ref": "https://github.com/git-suraj/flogo-components/activity/appendarraysv1",
+    "version": "0.0.1",
+    "title": "Append Arrays v1",
+    "description": "append two arrays",
+    "homepage":"https://github.com/git-suraj/flogo-components/tree/master/activity/appendarraysv1",
+    "settings":[
+    ],
+    "input":[
+      {
+        "name": "array1",
+        "type": "array",
+        "required": true
+      },
+      {
+        "name": "array2",
+        "type": "array",
+        "required": true
+      }
+    ],
+    "output": [
+      {
+        "name": "output",
+        "type": "array",
+        "required": true
+      }
+    ]
+  }
+  
+`
+
+// init create & register activity
+func init() {
+	md := activity.NewMetadata(jsonMetadata)
+	activity.Register(NewActivity(md))
+}
