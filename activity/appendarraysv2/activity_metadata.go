cb6dd8aa9443c5cf4896813264f29465014b2ab9
git-suraj <reach.suraj777@gmail.com>
2019-08-19 19:37:26 +0530

    working version of arrays concatenation

---
+++ activity/appendarraysv2/activity_metadata.go
@@ -0,0 +1,42 @@
+package appendarraysv2
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+var jsonMetadata = `{
+    "name": "tibco-append-arrays",
+    "type": "flogo:activity",
+    "ref": "github.com/git-suraj/flogo-components/activity/appendarraysv2",
+    "version": "0.1.1",
+    "title": "Append Arrays",
+    "description": "append two arrays",
+    "author": "suraj",
+    "homepage": "https://github.com/git-suraj/flogo-components/tree/master/activity/appendarraysv2",
+    "inputs": [
+    {
+      "name": "array1",
+      "type": "array",
+      "required": true
+    },
+    {
+      "name": "array2",
+      "type": "array",
+      "required": true
+    }
+  ],
+    "outputs": [
+       {
+      "name": "output",
+      "type": "array",
+      "required": true
+    }
+    ]
+}
+`
+
+// init create & register activity
+func init() {
+	md := activity.NewMetadata(jsonMetadata)
+	activity.Register(NewActivity(md))
+}
---
+++ activity/appendarraysv2/activity_test.go
@@ -0,0 +1,70 @@
+package appendarraysv2
+
+import (
+	"reflect"
+	"testing"
+
+	"io/ioutil"
+
+	"github.com/TIBCOSoftware/flogo-contrib/action/flow/test"
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+)
+
+var activityMetadata *activity.Metadata
+
+func getActivityMetadata() *activity.Metadata {
+
+	if activityMetadata == nil {
+		jsonMetadataBytes, err := ioutil.ReadFile("activity.json")
+		if err != nil {
+			panic("No Json Metadata found for activity.json path")
+		}
+
+		activityMetadata = activity.NewMetadata(string(jsonMetadataBytes))
+	}
+
+	return activityMetadata
+}
+
+func TestCreate(t *testing.T) {
+
+	act := NewActivity(getActivityMetadata())
+
+	if act == nil {
+		t.Error("Activity Not Created")
+		t.Fail()
+		return
+	}
+}
+
+func TestStructAppend(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Failed()
+			t.Errorf("panic during execution: %v", r)
+		}
+	}()
+
+	act := NewActivity(getActivityMetadata())
+	tc := test.NewTestActivityContext(getActivityMetadata())
+
+	//setup attrs
+	array1 := []request{
+		request{"a1n1", "a1v1"},
+		request{"a1n2", "a1v2"},
+	}
+	array2 := []request{
+		request{"a2n1", "a2v1"},
+		request{"a2n2", "a2v2"},
+	}
+	tc.SetInput("array1", array1)
+	tc.SetInput("array2", array2)
+
+	act.Eval(tc)
+	value := tc.GetOutput("output").([]request)
+	object := reflect.ValueOf(value)
+	if object.Len() != 4 {
+		t.Fail()
+	}
+}
